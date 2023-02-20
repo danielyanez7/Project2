d24902d88abd21b8bd9c42f6841026e0bb107312
Daniel Yanez <ingdanielyanez@gmail.com>
2023-02-20 20:58:09 +0100

    Create user - first conflict in merge

--- models/User.model.js
+++ models/User.model.js
@@ -41,7 +41,11 @@ const userSchema = new Schema(
     imageUrl: {
       type: String,
       default: "https://ih1.redbubble.net/image.2595320116.9420/flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
-    }
+    },
+    clients: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
   },
   {
     timestamps: true
